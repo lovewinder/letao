04c3c9aab42ae8dfec57700fe4b8c83c0e96fd0e
ye2k <529129461@qq.com>
2019-12-07 23:48:17 +0800

    index push

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -6,4 +6,26 @@ $(function () {
   $(document).ajaxStop(function () {
     NProgress.done();
   })
+
+  $(".top .slide").on("click", function () {
+    $(".aslid").toggleClass("slideLeft")
+    $(".top .slide").toggleClass("misPaddingLeft")
+  })
+
+  $(".top .quit").on("click", function () {
+    $(".modal").modal("show")
+  })
+
+  $(".quit-sure").on("click", function () {
+    location.href = "./login.html"
+  })
+
+  $(".doClass li:not('.notActive')").on("click", function () {
+    $(this).addClass("active")
+    $(this).siblings().removeClass("active")
+  })
+
+  $(".outUl>li.notActive").on("click", function () {
+    $(".sg").parent().slideToggle(1000)
+  })
 })
